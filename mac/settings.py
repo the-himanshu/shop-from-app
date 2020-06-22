c8e0f7f06820c464b9480271ab96277f2f806aaa
the-himanshu <56024176+the-himanshu@users.noreply.github.com>
2020-06-22 17:49:28 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "root"),]
+    os.path.join(BASE_DIR, "static"),]
 
 
 # Managing media
